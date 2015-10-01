676a3359c5d663d6121af99ea8bfda814cd70f0d
ramonlg <ramonlopes.87@gmail.com>
2015-10-01 17:32:38 +0000

    added pins scaffold, updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924173241) do
+ActiveRecord::Schema.define(version: 20150925184759) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
