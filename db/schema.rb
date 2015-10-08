0714745a922004472809a1421e2ff0465e67050d
ramonlg <ramonlopes.87@gmail.com>
2015-10-08 20:04:40 +0000

    added user name, updated devise forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005194301) do
+ActiveRecord::Schema.define(version: 20151007220614) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151005194301) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
